b774b9f4530eaa5fa6bd69483afb2d48ff290395
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-12 16:40:44 +0200

    FEAT active con navlink sui link attivi della navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 
 function Navbar() {
@@ -9,9 +9,15 @@ function Navbar() {
             <div className="container-fluid">
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item"><Link to='/'>Homepage</Link></li>
-                        <li className="nav-item"><Link to='/prodotti'>Prodotti</Link></li>
-                        <li className="nav-item"><Link to='/chi-siamo'>About Us</Link></li>
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to='/'>Homepage</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to='/prodotti'>Prodotti</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to='/chi-siamo'>About Us</NavLink>
+                        </li>
                     </ul>
                 </div>
             </div>
